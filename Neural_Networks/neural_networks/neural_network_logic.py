946bfe008bdfe75b66394672af7d1a0b4d29dab1
Donald Youmans <donald.youmans@unige.ch>
2024-04-08 16:05:19 +0200

    maintnance

--- Neural_Networks/neural_networks/neural_network_logic.py
+++ Neural_Networks/neural_networks/neural_network_logic.py
@@ -1,7 +1,5 @@
-# define neural network (for simplicity use 2 dense layers 2->3 and 3->1; learn (call activation) after each desne layer )
 import layer_logic as ll
 import numpy as np
-import time 
 
 class network:
     def __init__(self, layers : list[ll.Layer]):
@@ -9,33 +7,35 @@ class network:
 
     def predict(self, input : np.array) -> np.array:
         self.output = input
+
         for layer in self.layers:
             self.output = layer.forward(self.output)
+
         return self.output
-    
-    def learn(self, grad_output : np.array, learning_rate : float):
-        self.grad_output = grad_output
-        for layer in reversed(self.layers):
-            self.grad_output = layer.backward(self.grad_output,learning_rate)
-        return self.grad_output
 
     def train(self, loss, dloss, x_train : list[np.array], y_train : list[np.array], epochs : int, learning_rate : float, verbose : bool = True):
+        print('\n')
         for e in range(epochs):
-            self.error = 0
-            for x,y in zip(x_train,y_train):
+            error = 0.0
+            for i, (x,y) in enumerate(zip(x_train,y_train)):
                 
                 # forward propagation
                 self.y_hat = self.predict(x)
 
                 # update error
-                self.error += loss(y,self.y_hat)
+                error += loss(y,self.y_hat)
 
-                # backward propagation
-                self.grad = self.learn(dloss(y,self.y_hat),learning_rate)
+                # backward propagation 
+                self.grad_output = dloss(y,self.y_hat)
+
+                for layer in reversed(self.layers):
+                    self.grad_output = layer.backward(self.grad_output,learning_rate)
+                # self.learn(dloss(y,self.y_hat),learning_rate)
 
                 # normalize error 
-                self.error /= len(x_train)
+                error /= len(x_train)
 
                 if verbose:
-                    print(f"{e + 1}/{epochs}, error = {self.error:.5E}",end='\r')
+                    print(f"epoch {e + 1}/{epochs} {'':<10} sample {i+1}/{x_train.shape[0]} {'':<10} error = {self.error:.5e}",end='\r')
         print()
+
