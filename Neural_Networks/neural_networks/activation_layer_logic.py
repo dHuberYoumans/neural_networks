08bd3a15d02a6a2f045b4d3d281747c81856f328
Donald Youmans <donald.youmans@unige.ch>
2024-04-08 16:02:23 +0200

    added relu activation

--- Neural_Networks/neural_networks/activation_layer_logic.py
+++ Neural_Networks/neural_networks/activation_layer_logic.py
@@ -2,12 +2,20 @@
 import layer_logic as ll
 import numpy as np
 
+eps = 10**-5 # cut-off / regularization
+
 # strategy pattern for activation functions 
-class Activation(ll.Layer): # abstract activation layer
-    """ abstract strategy for activation layer """
+class Activation(ll.Layer): # abstract activation layer for LOCAL (f(x)_i = f(x_i)) activation functions
+    """ 
+    abstract strategy for activation layer of *local* activation functions.
+
+    A *local* activation functions is defined  as $f : \mathbb{R}^n \to \mathbb{R}^n$ with $f(x)_i = f(x_i)$.
+
+    Examples are tanh(x), sigmoid(x) etc. A counter example is softmax(x) since the denominator considers information of all $x_i$ 
+    """
     def __init__(self, activation, dactivation):
         self.activation = activation # need to pass functions, e.g. lambda functions
-        self.dactivation = dactivation
+        self.dactivation = dactivation 
 
     def forward(self, input : np.array) -> np.array:
         self.input = input
@@ -15,31 +23,38 @@ class Activation(ll.Layer): # abstract activation layer
 
     def backward(self, grad_output : np.array, learning_rate : float) -> np.array:
         self.grad_E = grad_output
-        return np.multiply(self.grad_E,self.dactivation(self.input))
+        return np.multiply(self.dactivation(self.input),self.grad_E)
     
 class Tanh(Activation): # concrete activation layer
-     """ concrete strategy for activation layer 
-     
-     implements tanh(x) as activation function
+     """ 
+     implements tanh activation
      """
      def __init__(self):
         tanh = lambda x : np.tanh(x)
-        dtanh = lambda x : 1 - np.tanh(x)**2
+        dtanh = lambda x : (1 - np.tanh(x)**2)
         super().__init__(tanh,dtanh)
 
 class Sigmoid(Activation): # concrete activation layer
-    """ concrete strategy for activation layer 
-    
-    implements sigmoid function s(x) = 1 / (1 + exp(-x)) as activation function
+    """ 
+    implements sigmoid activation s(x) = 1 / (1 + exp(-x)) 
     """
     def __init__(self):
         sigmoid = lambda x : 1 / (1 + np.exp(-x))
         dsigmoid = lambda x : sigmoid(x)*(1 - sigmoid(x))
         super().__init__(sigmoid, dsigmoid)
+
+
     
 class ReLu(Activation):
+    """
+    implements rectified linear unit (relu) activation relu(x) = max(epsilon,x)
+
+    the regularization epsilon is set to 0.01
+    """
     def __init__(self):
         epsilon = 0.01
         relu = lambda x : np.maximum(x,epsilon*np.ones(x.shape))
         drelu = lambda x : np.piecewise(x, [x < 0, x >= 0], [epsilon, 1])
         super().__init__(relu, drelu)
+
+
